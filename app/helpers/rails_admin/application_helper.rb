9ec22c1f170360c252fc43c7998c7a3552f3131a
C80609A <c080609a@gmail.com>
2022-08-11 17:05:04 +0300

    v.2.1.1: Sidebar navigation patches: меню в обратном порядке:
      сначала выводим блок Addition, затем модели, которые
      заключены в свои схемы, в конце - модели в схеме public.

--- app/helpers/rails_admin/application_helper.rb
+++ app/helpers/rails_admin/application_helper.rb
@@ -81,7 +81,7 @@ module RailsAdmin
         label = navigation_label || t('admin.misc.navigation')
 
         %(<li class='dropdown-header'>#{capitalize_first_letter label}</li>#{li_stack}) if li_stack.present?
-      end.join.html_safe
+      end.reverse.join.html_safe
     end
 
     def root_navigation
